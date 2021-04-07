35fb7e747bb51f5ecaab84c55c9a02e7bc65579d
michellewt68 <michellewt68@gmail.com>
2021-04-07 13:20:52 +0700

    nambah map and filter

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hellooo')
+//Ambil element
+const skills= document.getElementById('skills')
+const primaryskill=document.getElementById('primarySkill')
+
+const mySkills= ["ES6", " GIT"," Web-dev"]
+
+//undefined --> salah
+const printSkillsForEach=mySkills.forEach((skill)=>{
+    return skill
+})
+
+//benar
+const printSkillsMap=mySkills.map((skill)=>{
+    return skill
+})
+
+skills.innerHTML=printSkillsMap;
+
+//filter
+const printPrimarySkill=mySkills.filter((skill)=>{
+    return skill== 'ES6'
+})
+
+primaryskill.innerHTML=printPrimarySkill
