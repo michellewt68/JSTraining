3e087739ac24e0d110a1d416a8635d221dad8608
michellewt68 <michellewt68@gmail.com>
2021-04-07 10:24:10 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello")
+
+//ES5
+var myName="Ariq"
+myName="Tan"
+console.log(myName)
+
+//ES6
+//const/constant --> gabisa ditimpa jadi error 
+
+const myCar="mazda"
+// const myCar="Honda"
+// myCar="toyota"
+console.log(myCar)
