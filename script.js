a342d33e9b3bc0a64cfd9bf19fd6b9723d36048d
michellewt68 <michellewt68@gmail.com>
2021-04-07 11:22:41 +0700

    object literals

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hellooo')
+//object literals
+const cart=document.getElementById("cart");
+
+//ES6
+function addproduct(name, category){
+    return {
+        name,category
+    };
+}
+
+var getProduct=addproduct("ip12","smartphone");
+console.log(getProduct);
+
+cart.innerHTML=`Product 1: ${getProduct.name}, category ${getProduct.category}`;
