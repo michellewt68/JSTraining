c65f86f4603684f2ff8002e44034a4db3a5ca427
michellewt68 <michellewt68@gmail.com>
2021-04-07 10:37:43 +0700

    init master

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-console.log("hello")
-
-//ES5
-var myName="Ariq"
-myName="Tan"
-console.log(myName)
-
-//ES6
-//const/constant --> gabisa ditimpa jadi error 
-
-const myCar="mazda"
-// const myCar="Honda"
-// myCar="toyota"
-console.log(myCar)
+console.log('hellooo')
