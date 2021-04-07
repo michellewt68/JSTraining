437de4f75a4b1c44e579fb6102fb7c1da3f1fe59
michellewt68 <michellewt68@gmail.com>
2021-04-07 11:06:57 +0700

    belajar string concat

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('hellooo')
+// console.log("hello")
+
+// //ES5
+// var myName="Ariq"
+// myName="Tan"
+// console.log(myName)
+
+// //ES6
+// //const/constant --> gabisa ditimpa jadi error 
+
+// const myCar="mazda"
+// // const myCar="Honda"
+// // myCar="toyota"
+// console.log(myCar)
+
+//==================
+//LET
+//ES5
+// var myName="Ari"
+// myName="Ricad"
+// console.log(myName)
+
+var mycarsVar=["BMW","Tesla","Toyota"]
+for(var i=0;i<mycarsVar.length;i++){
+    console.log("My cars var= "+mycarsVar[i] )
+}
+
+console.log(i)
+
+//ES6
+// let mynameLet="Ari"
+// mynameLet="Calvin"
+// console.log(mynameLet)
+
+const mycarsLet=["BMW","Tesla","Toyota"]
+for(let x=0;x<mycarsLet.length;x++){
+    console.log("My cars let = "+mycarsLet[x] )
+}
+
+// console.log(x)
+
+//var bisa dipake di semua , kalo let cuma bisa di lingkupnya / scope loopingny doang  {}
